fd6471d9d606a8f7a7c6e2134d1fe55a402b487e
Santiago Nicolás Contrera <saantyago.contrera@gmail.com>
2023-01-17 17:11:47 -0300

    Se agregó al proyecto la vista de búsqueda, andando y todo

---
+++ mi_app/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+
+class Cursoformulario(forms.Form):
+
+    nombre=forms.CharField(max_length=40)
+    camada = forms.IntegerField()
+
+
+
+class Estudianteformulario(forms.Form):
+    nombre= forms.CharField(max_length=30)
+    apellido= forms.CharField(max_length=30)
+    email= forms.EmailField()
+
+class Profesoresformulario(forms.Form):
+    nombre= forms.CharField(max_length=30)
+    apellido= forms.CharField(max_length=30)
+    email= forms.EmailField()
+    profesion= forms.CharField(max_length=30)
+
+class Entregablesformulario(forms.Form):
+    nombre= forms.CharField(max_length=30)
+    fechaDeEntrega = forms.DateField()  
+    entregado = forms.BooleanField()
